500cf887b4f4c891fbf8ec8e80a7af4e986e6733
Sultan <sultanfastakhiev@gmail.com>
2025-05-22 11:06:38 +0300

    update

--- bot/app/handlers/v1/errors.py
+++ bot/app/handlers/v1/errors.py
@@ -6,28 +6,26 @@ from app.core.bot import bot
 
 router = Router()
 
-@router.errors()
-async def global_error_handler(update: Update, exception: Exception):
+@router.error()
+async def global_error_handler(event: Update, exception: Exception) -> bool:
     if isinstance(exception, TelegramBadRequest):
         error_text = str(exception)
 
-        if "message can't be edited" in error_text or "message to edit not found" in error_text or "message can't be deleted" in error_text:
-            if update.callback_query:
-                user = update.callback_query.from_user
-                chat = update.callback_query.message.chat
-                message_id = update.callback_query.message.message_id
+        if "message can't be edited" in error_text or "message to edit not found" in error_text:
+            if event.callback_query:
+                user = event.callback_query.from_user
+                chat = event.callback_query.message.chat
+                message_id = event.callback_query.message.message_id
 
                 fake_message = Message(
                     message_id=message_id,
-                    date=update.callback_query.message.date,
+                    date=event.callback_query.message.date,
                     chat=chat,
                     from_user=user,
-                    text="/start",
-                    message_thread_id=None
+                    text="/start"
                 )
 
                 state = await router.fsm.get_context(bot, chat.id, user.id)
-
                 await cmd_start(fake_message, state=state)
 
             return True
