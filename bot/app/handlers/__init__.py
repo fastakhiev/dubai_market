2579c827682755abf20ed341b9dd8367ac12e5b8
Sultan <sultanfastakhiev@gmail.com>
2025-05-22 11:01:43 +0300

    include router

--- bot/app/handlers/__init__.py
+++ bot/app/handlers/__init__.py
@@ -4,6 +4,7 @@ from app.handlers.v1.sellers import router as sellers_router
 from app.handlers.v1.search import router as search_router
 from app.handlers.v1.buyers import router as buyers_router
 from app.handlers.v1.common import router as common_router
+from app.handlers.v1.errors import router as error_router
 
 router = Router()
 router.include_router(sign_up_router)
@@ -11,3 +12,4 @@ router.include_router(sellers_router)
 router.include_router(search_router)
 router.include_router(buyers_router)
 router.include_router(common_router)
+router.include_router(error_router)
