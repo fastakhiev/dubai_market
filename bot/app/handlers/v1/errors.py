8d7cacc7c15fd8ad598efa904871898992c4879b
Sultan <sultanfastakhiev@gmail.com>
2025-05-22 18:04:12 +0300

    update

--- bot/app/handlers/v1/errors.py
+++ bot/app/handlers/v1/errors.py
@@ -1,13 +1,11 @@
-from aiogram import Router
 from aiogram.types import Update, Message
 from aiogram.exceptions import TelegramBadRequest
 from app.handlers.v1.sign_up import cmd_start
 from app.core.bot import bot
+from aiogram import Dispatcher
 
-router = Router()
 
-@router.error()
-async def global_error_handler(event: Update, exception: Exception):
+async def global_error_handler(update: Update, exception: Exception) -> bool:
     if isinstance(exception, TelegramBadRequest):
         error_text = str(exception)
 
@@ -16,20 +14,20 @@ async def global_error_handler(event: Update, exception: Exception):
             or "message to edit not found" in error_text
             or "message can't be deleted for everyone" in error_text
         ):
-            if event.callback_query:
-                user = event.callback_query.from_user
-                chat = event.callback_query.message.chat
-                message_id = event.callback_query.message.message_id
+            if update.callback_query:
+                user = update.callback_query.from_user
+                chat = update.callback_query.message.chat
+                message_id = update.callback_query.message.message_id
 
                 fake_message = Message(
                     message_id=message_id,
-                    date=event.callback_query.message.date,
+                    date=update.callback_query.message.date,
                     chat=chat,
                     from_user=user,
                     text="/start"
                 )
 
-                state = await router.fsm.get_context(bot, chat.id, user.id)
+                state = await Dispatcher.get_current().fsm.get_context(bot, chat.id, user.id)
                 await cmd_start(fake_message, state=state)
 
             return True
