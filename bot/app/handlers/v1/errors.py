ef1bb8e8909f424213a1999b770bc6bd62e1a984
Sultan <sultanfastakhiev@gmail.com>
2025-05-22 17:30:47 +0300

    update error

--- bot/app/handlers/v1/errors.py
+++ bot/app/handlers/v1/errors.py
@@ -1,28 +1,33 @@
+from aiogram import Dispatcher
 from aiogram.types import Update, Message
 from aiogram.exceptions import TelegramBadRequest
 from app.handlers.v1.sign_up import cmd_start
 from app.core.bot import bot
 
-
-async def global_error_handler(event: Update, exception: Exception) -> bool:
+async def global_error_handler(update: Update, data: dict, exception: Exception):
     if isinstance(exception, TelegramBadRequest):
         error_text = str(exception)
 
-        if "message can't be edited" in error_text or "message to edit not found" in error_text or "message can't be deleted for everyone" in error_text:
-            if event.callback_query:
-                user = event.callback_query.from_user
-                chat = event.callback_query.message.chat
-                message_id = event.callback_query.message.message_id
+        if (
+            "message can't be edited" in error_text
+            or "message to edit not found" in error_text
+            or "message can't be deleted for everyone" in error_text
+        ):
+            if update.callback_query:
+                user = update.callback_query.from_user
+                chat = update.callback_query.message.chat
+                message_id = update.callback_query.message.message_id
 
                 fake_message = Message(
                     message_id=message_id,
-                    date=event.callback_query.message.date,
+                    date=update.callback_query.message.date,
                     chat=chat,
                     from_user=user,
                     text="/start"
                 )
 
-                state = await router.fsm.get_context(bot, chat.id, user.id)
+                dp: Dispatcher = data["dispatcher"]
+                state = await dp.fsm.get_context(bot, chat.id, user.id)
                 await cmd_start(fake_message, state=state)
 
             return True
